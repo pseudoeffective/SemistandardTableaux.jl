31229c8fa5a2aa99aa28b4b328850d6338fd2113
David Anderson <david.ed.anderson@gmail.com>
2026-05-31 08:38:01 -0400

    initial commit

--- src/SemistandardTableaux.jl
+++ src/SemistandardTableaux.jl
@@ -1,5 +1,61 @@
-module SchurPolynomials
+################################################################################
+# SemistandardTableaux.jl
+#
+# A Semistandard Tableaux package for Julia.
+#
+# Copyright (C) 2026 Dave Anderson, pseudoeffective.github.io
+################################################################################
 
-greet() = print("Hello World!")
 
-end # module SchurPolynomials
+module SemistandardTableaux
+
+################################################################################
+# Import
+################################################################################
+
+# Base
+import Base:
+	*, transpose
+
+# Combinatorics
+import Combinatorics:
+	nthperm
+
+# AbstractAlgebra
+import AbstractAlgebra:
+	base_ring, gen, gens, parent_type, nvars, polynomial_ring, MPolyBuildCtx, push_term!, finish
+
+# Nemo
+import Nemo:
+	ZZ, QQ, libflint, ZZMPolyRing, ZZMPolyRingElem, evaluate, vars, coefficients
+
+# LinearAlgebra for determinant
+import LinearAlgebra: 
+	det
+
+# Memoization
+import Memoization: 
+	@memoize
+
+
+# bpds
+using BumplessPipeDreams
+
+################################################################################
+# Export (more exports are in the source files)
+################################################################################
+
+export
+	ZZ, QQ, PolyRing, ZZMPolyRing, ZZMPolyRingElem, QQMPolyRing, QQMPolyRingElem
+	
+
+
+
+################################################################################
+# source files
+################################################################################
+
+include("ssyt.jl")
+
+
+end
